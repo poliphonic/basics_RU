d5cca26a0fdfbf70d6097ffd231ef2e4e8a48a2c
poliphonic <73651126+poliphonic@users.noreply.github.com>
2021-12-21 00:00:06 +0300

    20 December 2021

---
+++ guess_num.py
@@ -0,0 +1,15 @@
+import random
+
+
+def person_guess():
+    number = random.randint(1, 100)
+    print('Угадай число от 1 до 100.')
+    guess = int(input())
+    while guess != number:
+        print('>' if number > guess else '<')
+        guess = int(input())
+    print('Победа! Загаданное число {}.'.format(number))
+
+
+if __name__ == '__main__':
+    person_guess()
---
+++ music_deserialize.py
@@ -0,0 +1,8 @@
+# Создать модуль music_deserialize.py. В этом модуле открыть файлы .json и .pickle, прочитать из них информацию. И
+# получить объект: словарь из предыдущего задания.
+import pickle
+import json
+with open('son_lux.pickle', 'rb') as fp:
+    print(pickle.load(fp))
+with open('son_lux.json', 'r') as fj:
+    print(json.load(fj))
